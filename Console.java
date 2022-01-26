0ed156dc1f82ab96c5abaf864d0a94fecd801a64
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2022-01-25 23:56:06 -0700

    Console.java
    The basic console java part. Makes code look nice

---
+++ Console.java
@@ -0,0 +1,13 @@
+package edu.neumont.math;
+
+public class Console {
+
+    public void console() {
+        System.out.printf("Math Solver Loaded");
+        consoleLog();
+    }
+
+    public void consoleLog() {
+        System.out.printf("\n----------------------------------------\n");
+    }
+}
